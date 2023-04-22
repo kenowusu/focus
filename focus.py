07f29d260868a6a63b07a7245b924188d275b32d
Kenneth Owusu <k.owusu.me@gmail.com>
2023-04-22 21:41:44 +0000

    build: nothing chages, fix

--- focus.py
+++ focus.py
@@ -1,31 +1,40 @@
 import os
+from sty import fg, bg, ef, rs
 
 
+#get host file path
 hostpath = os.path.join('c:\\','Windows','System32','drivers','etc','hosts')
 
+#web domain list to turn on and off
 weblist = [
     'www.youtube.com',
     'youtube.com'
 ]
 
+#focus function to turn on and off focus mode
 def focus(focusmode):
     
     if focusmode == 'on':
 
         try:
-
+            #open host file in read mode and read all lines
             with open(hostpath,mode='r') as openhostfile:
                 lines = openhostfile.readlines()
-
+                
+                #open host file in write mode
                 with open(hostpath,mode='w') as hostfile:
 
                     for line in lines:
-                        print(line)
                         if  line.lstrip('#127.0.0.1 ').rstrip('\n') in weblist:
                             hostfile.write(line.lstrip('#'))
-                           
+                        
+                        #just write domain to file
                         else:
                             hostfile.write(line)
+                    print(fg.green + 'Focus mode is on' + fg.rs)
+                           
+
+                        
                           
         except Exception as e:
             print(e)
@@ -42,12 +51,15 @@ def focus(focusmode):
 
                     for line in lines:
                         if  line.lstrip('#127.0.0.1 ').rstrip('\n') in weblist:
-                            hostfile.write('#'+line)
-     
+                            striped_line = line.lstrip('#127.0.0.1 ')
+                            hostfile.write('#127.0.0.1 '+striped_line)
                         
+                        #just write domain to file
                         else:
                             hostfile.write(line)
+                    print(fg.red + 'Focus mode is off' + fg.rs)
+
+     
+
         except Exception as e:
             print(e)
-
-focus('on')
